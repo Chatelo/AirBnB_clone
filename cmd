4feada40c51476457d324cad505e9d6287d56a61
Benard Ronoh <biikate48.com>
2023-08-12 23:43:58 -0700

    beautiful codes

--- cmd
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python3
-
-print("Write beautiful code that passes the pycodestyle checks")
---
+++ models/base_model.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Defines the BaseModel class."""
+from datetime import datetime
+from uuid import uuid4
+
+import models
+
+
+class BaseModel:
+    """Represents the BaseModel of the HBnB project."""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize a new BaseModel.
+
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
+        """
+        tform = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
